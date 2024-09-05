31dbfbd9d7e3810664a2c1cca129e88748e29413
KiwiScreams <screamskiwi3@gmail.com>
2024-09-05 17:03:53 +0400

    navigation

--- src/components/shared/header/nav/Nav.jsx
+++ src/components/shared/header/nav/Nav.jsx
@@ -2,6 +2,7 @@ import "./Nav.css";
 import iconBrowse from "../../../../assets/icons/icon-browse.svg";
 import { Link, NavLink } from "react-router-dom";
 import iconHot from "../../../../assets/icons/header icons/icon-hot.svg.svg";
+import iconHeadphone from "../../../../assets/icons/header icons/icon-headphone.svg.svg";
 const navLinks = [
   { to: "/deals", icon: iconHot, label: "Deals" },
   { to: "/", label: "Home" },
@@ -26,7 +27,12 @@ const Nav = () => {
             <ul>
               {navLinks.map((link) => (
                 <li key={link.to}>
-                  <NavLink to={link.to}>
+                  <NavLink
+                    to={link.to}
+                    className={({ isActive }) =>
+                      isActive ? "active-link" : ""
+                    }
+                  >
                     {link.icon && <img src={link.icon} alt="" />}
                     {link.label}
                   </NavLink>
@@ -34,6 +40,13 @@ const Nav = () => {
               ))}
             </ul>
           </nav>
+          <div className="support-center">
+            <img src={iconHeadphone} alt="" />
+            <div>
+                <span>1900 - 888</span>
+                <span>24/7 Support Center</span>
+            </div>
+          </div>
         </div>
       </section>
     </>
