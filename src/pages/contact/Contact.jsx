bbc03b9dd21e919fa663b40a56ee2aa9ccc942ac
KiwiScreams <screamskiwi3@gmail.com>
2024-09-05 22:30:10 +0400

    contact page

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,8 +1,55 @@
 import "./Contact.css";
 const Contact = () => {
-    return ( 
-        <><h1>contact</h1></>
-     );
-}
- 
+  return (
+    <>
+      <section className="contact-section">
+        <span className="green-txt">How can help you ?</span>
+        <div className="container">
+          <div className="article">
+            <h2>Let us know how we can help you</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+              tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+              tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+            </p>
+          </div>
+          <div className="articles-container">
+            <div className="article-content">
+              <h4>01. feedback</h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+                tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+              </p>
+            </div>
+            <div className="article-content">
+              <h4>01. feedback</h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+                tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+              </p>
+            </div>
+            <div className="article-content">
+              <h4>01. feedback</h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+                tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+              </p>
+            </div>
+            <div className="article-content">
+              <h4>01. feedback</h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+                tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
 export default Contact;
