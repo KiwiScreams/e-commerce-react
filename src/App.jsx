6d9b372e9a64daa0c58c24afa016576d18b16102
KiwiScreams <screamskiwi3@gmail.com>
2024-09-05 14:49:21 +0400

    header top

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,13 @@ import Contact from "./pages/contact/Contact";
 import Wishlist from "./pages/wishlist/Wishlist";
 import Cart from "./pages/cart/Cart";
 import NotFound from "./pages/not found/NotFound";
+import Header from "./components/shared/header/Header";
+import Footer from "./components/shared/footer/Footer";
 
 function App() {
   return (
     <>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
@@ -17,6 +20,7 @@ function App() {
         <Route path="/cart" element={<Cart />}></Route>
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
+      <Footer />
     </>
   );
 }
--- src/components/shared/header/Header.jsx
+++ src/components/shared/header/Header.jsx
@@ -1,8 +1,38 @@
 import "./Header.css";
+import { Link } from "react-router-dom";
 const Header = () => {
-    return ( 
-        <><h1>header</h1></>
-     );
-}
- 
+  return (
+    <>
+      <header>
+        <div className="header-top">
+          <div className="header-top-container">
+            <ul className="header-top-nav">
+              <li>
+                <Link to="/about">About Us</Link>
+              </li>
+              <li>
+                <Link to="/">My Account</Link>
+              </li>
+              <li>
+                <Link to="/wishlist">Wishlist</Link>
+              </li>
+              <li>
+                <Link to="/wishlist">Order Tracking</Link>
+              </li>
+            </ul>
+            <p>100% Secure delivery without contacting the courier</p>
+            <ul className="header-top-nav">
+              <li>
+                Need help? Call Us:<span className="green-txt">+ 1800 900</span>
+              </li>
+              <li>English</li>
+              <li>USD</li>
+            </ul>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
 export default Header;
