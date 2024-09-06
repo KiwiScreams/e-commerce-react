769ac847d09ecb242e9607a39d72efa16b942523
KiwiScreams <screamskiwi3@gmail.com>
2024-09-06 16:12:00 +0400

    footer

--- src/components/shared/footer/Footer.jsx
+++ src/components/shared/footer/Footer.jsx
@@ -6,34 +6,50 @@ import iconLocation from "../../../assets/icons/footer icons/icon-location.svg.s
 import iconHeadphone from "../../../assets/icons/footer icons/icon-contact.svg.svg";
 import iconEmail from "../../../assets/icons/footer icons/icon-email.svg.svg";
 import iconClock from "../../../assets/icons/footer icons/icon-clock.svg.svg";
+const footerContacts = [
+  {
+    id: 1,
+    image: iconLocation,
+    title: "Address: ",
+    description: "5171 W Campbell Ave undefined Kent, Utah 53127 United States",
+  },
+  {
+    id: 2,
+    image: iconHeadphone,
+    title: "Call Us: ",
+    description: "(+91) - 540-025-124553",
+  },
+  {
+    id: 3,
+    image: iconEmail,
+    title: "Email: ",
+    description: "sale@Nest.com",
+  },
+  {
+    id: 4,
+    image: iconClock,
+    title: "Hours: ",
+    description: "10:00 - 18:00, Mon - Sat",
+  },
+];
 const Footer = () => {
   return (
     <>
       <footer>
         <div className="footer-top">
           <div className="footer-container">
-            <img src={logo} alt="" className="logo"/>
+            <img src={logo} alt="" className="logo" />
             <p className="under-logo">Awesome grocery store website template</p>
             <ul>
-              <li>
-                <img src={iconLocation} alt="" />
-                <p>
-                  <span>Address:</span> 5171 W Campbell Ave undefined Kent, Utah
-                  53127 United States
-                </p>
-              </li>
-              <li>
-                <img src={iconHeadphone} alt="" />
-                Call Us:(+91) - 540-025-124553
-              </li>
-              <li>
-                <img src={iconEmail} alt="" />
-                Email:sale@Nest.com
-              </li>
-              <li>
-                <img src={iconClock} alt="" />
-                Hours:10:00 - 18:00, Mon - Sat
-              </li>
+              {footerContacts.map((footerContact) => (
+                <li key={footerContact.id}>
+                  <img src={footerContact.image} alt="" />
+                  <p>
+                    <span>{footerContact.title}</span>
+                    {footerContact.description}
+                  </p>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
