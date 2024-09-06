2238a9b8e7026f92192bbfb6c2fd25b817892fde
KiwiScreams <screamskiwi3@gmail.com>
2024-09-06 23:35:50 +0400

    price range style

--- src/components/side bar/filter/Filter.jsx
+++ src/components/side bar/filter/Filter.jsx
@@ -49,93 +49,85 @@ const Filter = () => {
   return (
     <section className="filter-section">
       <h2>Filter by price</h2>
-      <ul>
-        <li className="category-li">
-          <div className="image-container">
-            {/* <img src={category.image} alt="" /> */}
-          </div>
-          <div className="price-range">
-            <div
-              id="slider"
-              style={{
-                position: "relative",
-                maxWidth: "200px",
-                height: "6px",
-                backgroundColor: "#ccc", // Change to gray
-              }}
-            >
-              <div
-                style={{
-                  position: "absolute",
-                  top: 0,
-                  left: 0,
-                  width: `${thumbLeft}px`,
-                  maxWidth: "200px",
-                  height: "6px",
-                  backgroundColor: "#ccc", // Change to gray
-                }}
-              />
-              <div
-                style={{
-                  position: "absolute",
-                  top: 0,
-                  left: `${thumbLeft}px`,
-                  width: `${Math.min(
-                    thumbRight - thumbLeft,
-                    200 - thumbLeft
-                  )}px`, // Add this line
-                  height: "6px",
-                  backgroundColor: "#ccc", // Change to gray
-                }}
-              />
-              <div
-                style={{
-                  position: "absolute",
-                  top: 0,
-                  left: `${thumbRight}px`,
-                  width: `${Math.min(200 - thumbRight, 200 - thumbRight)}px`, // Update width calculation
-                  height: "6px",
-                  backgroundColor: "#ccc", // Change to gray
-                }}
-              />
-              <div
-                className="thumb left-thumb"
-                style={{
-                  position: "absolute",
-                  top: "-5px",
-                  left: `${thumbLeft}px`,
-                  width: "10px",
-                  height: "10px",
-                  borderRadius: "50%",
-                  backgroundColor: "#34C759", // Change to green
-                  cursor: "pointer",
-                }}
-                onMouseDown={(e) => handleMouseDown(e, "left")}
-              />
-              <div
-                className="thumb right-thumb"
-                style={{
-                  position: "absolute",
-                  top: "-5px",
-                  left: `${thumbRight}px`,
-                  width: "10px",
-                  height: "10px",
-                  borderRadius: "50%",
-                  backgroundColor: "#34C759", // Change to green
-                  cursor: "pointer",
-                }}
-                onMouseDown={(e) => handleMouseDown(e, "right")}
-              />
-            </div>
-            <output style={{ display: "block", fontSize: "16px" }}>
-              Min price: ${minPrice}
-            </output>
-            <output style={{ display: "block", fontSize: "16px" }}>
-              Max price: ${maxPrice}
-            </output>
-          </div>
-        </li>
-      </ul>
+      <div className="price-range">
+        <div
+          id="slider"
+          style={{
+            position: "relative",
+            maxWidth: "236px",
+            height: "6px",
+            backgroundColor: "#D6D7D9",
+          }}
+        >
+          <div
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: `${thumbLeft}px`,
+              maxWidth: "236px",
+              height: "6px",
+              backgroundColor: "#3BB77E",
+            }}
+          />
+          <div
+            style={{
+              position: "absolute",
+              top: 0,
+              left: `${thumbLeft}px`,
+              width: `${Math.min(thumbRight - thumbLeft, 236 - thumbLeft)}px`,
+              height: "6px",
+              backgroundColor: "#3BB77E",
+            }}
+          />
+          <div
+            style={{
+              position: "absolute",
+              top: 0,
+              left: `${thumbRight}px`,
+              width: `${Math.min(236 - thumbRight, 236 - thumbRight)}px`,
+              height: "6px",
+              backgroundColor: "#D6D7D9",
+            }}
+          />
+          <div
+            className="thumb left-thumb"
+            style={{
+              position: "absolute",
+              top: "-5px",
+              left: `${thumbLeft}px`,
+              width: "18px",
+              height: "18px",
+              borderRadius: "50%",
+              backgroundColor: "#3BB77E",
+              cursor: "pointer",
+            }}
+            onMouseDown={(e) => handleMouseDown(e, "left")}
+          />
+          <div
+            className="thumb right-thumb"
+            style={{
+              position: "absolute",
+              top: "-5px",
+              left: `${thumbRight}px`,
+              width: "18px",
+              height: "18px",
+              borderRadius: "50%",
+              backgroundColor: "#3BB77E",
+              cursor: "pointer",
+            }}
+            onMouseDown={(e) => handleMouseDown(e, "right")}
+          />
+        </div>
+        <div className="flex">
+          <output style={{ display: "block", fontSize: "16px" }}>
+            From: <span className="green-txt">${minPrice}</span>
+          </output>
+          <output style={{ display: "block", fontSize: "16px" }}>
+            To: <span className="green-txt">${maxPrice}</span>
+          </output>
+        </div>
+      </div>
     </section>
   );
 };
