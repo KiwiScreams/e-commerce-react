240cb41fb16445bdacc719581aa34467fe4275fb
KiwiScreams <screamskiwi3@gmail.com>
2024-09-06 21:41:31 +0400

    categories

--- src/components/side bar/category/Category.jsx
+++ src/components/side bar/category/Category.jsx
@@ -1,10 +1,21 @@
 import "./Category.css";
+import categories from "..";
 const Category = () => {
-    return ( 
-        <>
-        <h1>category</h1>
-        </>
-     );
-}
- 
+  return (
+    <>
+      <section className="category-section">
+        <h2>Category</h2>
+        <ul>
+          {categories.map((category) => (
+            <li key={category.id}>
+              <img src={category.image} alt="" />
+              {category.title}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </>
+  );
+};
+
 export default Category;
---
+++ src/components/side bar/index.js
@@ -0,0 +1,51 @@
+import categoryIcon1 from "../../assets/icons/category icons/category-1.svg.svg";
+import categoryIcon2 from "../../assets/icons/category icons/category-2.svg.svg";
+import categoryIcon3 from "../../assets/icons/category icons/category-3.svg.svg";
+import categoryIcon4 from "../../assets/icons/category icons/category-4.svg.svg";
+import categoryIcon5 from "../../assets/icons/category icons/category-5.svg.svg";
+import categoryIcon6 from "../../assets/icons/category icons/category-6.svg.svg";
+import categoryIcon7 from "../../assets/icons/category icons/category-7.svg.svg";
+import categoryIcon8 from "../../assets/icons/category icons/category-8.svg.svg";
+const categories = [
+  {
+    id: 1,
+    title: "Milks & Dairies",
+    image: categoryIcon1
+  },
+  {
+    id: 2,
+    title: "Wines & Alcohol",
+    image: categoryIcon2
+  },
+  {
+    id: 3,
+    title: "Clothing & Beauty",
+    image: categoryIcon3
+  },
+  {
+    id: 4,
+    title: "Pet Foods & Toy",
+    image: categoryIcon4
+  },
+  {
+    id: 5,
+    title: "Packaged fast food",
+    image: categoryIcon5
+  },
+  {
+    id: 6,
+    title: "Baking material",
+    image: categoryIcon6
+  },
+  {
+    id: 7,
+    title: "Vegetables & tubers",
+    image: categoryIcon7
+  },
+  {
+    id: 8,
+    title: "Fresh Seafood",
+    image: categoryIcon8
+  },
+];
+export default categories;
