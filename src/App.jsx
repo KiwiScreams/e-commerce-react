d3101a8c0759c3d803fe32be104f8e654dbfb570
KiwiScreams <screamskiwi3@gmail.com>
2024-09-07 15:31:55 +0400

    detail page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Cart from "./pages/cart/Cart";
 import NotFound from "./pages/not found/NotFound";
 import Header from "./components/shared/header/Header";
 import Footer from "./components/shared/footer/Footer";
+import Detail from "./pages/detail/Detail";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/product:id" element={<Detail />} />
         <Route path="/about" element={<About />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
         <Route path="/wishlist" element={<Wishlist />}></Route>
--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -1,7 +1,20 @@
 import "./Detail.css";
+import { useParams } from "react-router-dom";
 const Detail = () => {
+    const { id } = useParams();
+    const [product, setProducts] = useState([]);
+    useEffect(() => {
+      axios
+        .get("http://localhost:8000/products/" + id)
+        .then((res) => setProducts(res.data))
+        .catch((err) => console.log(err));
+    }, []);
     return ( 
-        <><h1>detail</h1></>
+        <>
+        <h1>detail</h1>
+        <h1>{product.id}</h1>
+        <h1>{product.name}</h1>
+        </>
      );
 }
  
