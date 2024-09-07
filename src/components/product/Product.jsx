3610499466752c75615f2860fda63111dd26dba4
KiwiScreams <screamskiwi3@gmail.com>
2024-09-07 13:04:54 +0400

    product list

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,27 +1,10 @@
 import "./Product.css";
-import axios from "axios";
-import { useState, useEffect } from "react";
-const Product = () => {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get("http://localhost:8000/products")
-      .then((res) => setData(res.data))
-      .catch((err) => console.log(err));
-  }, []);
+const Product = ({product}) => {
   return (
     <>
       <section className="product-section">
-        <h2>Products</h2>
-        <ul>
-          {data.map((product) => (
-            <li key={product.id}>
-              <h3>{product.name}</h3>
-              <p>{product.description}</p>
-            </li>
-          ))}
-        </ul>
+        <h3>{product.name}</h3>
+        <p>{product.id}</p>
       </section>
     </>
   );
