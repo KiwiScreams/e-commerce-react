b1ddbf4dc7831aa4f5798774b2e6be613aa473d7
KiwiScreams <screamskiwi3@gmail.com>
2024-09-07 12:53:57 +0400

    create product components

---
+++ src/components/products list/ProductsList.jsx
@@ -0,0 +1,12 @@
+import "./ProductsList.css";
+const ProductsList = () => {
+    return ( 
+        <>
+        <section className="product-list-section">
+            
+        </section>
+        </>
+     );
+}
+ 
+export default ProductsList;
