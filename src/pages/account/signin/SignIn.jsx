f670f5790f7dc1636fe0e3daf892452f41b2561e
KiwiScreams <screamskiwi3@gmail.com>
2024-09-08 15:41:34 +0400

    login

--- src/pages/account/signin/SignIn.jsx
+++ src/pages/account/signin/SignIn.jsx
@@ -1,20 +1,36 @@
 import { Link, useNavigate } from "react-router-dom";
 import "./SignIn.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const SignIn = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [emailError, setEmailError] = useState("");
   const [passwordError, setPasswordError] = useState("");
+  const [loginError, setLoginError] = useState("");
+  const [users, setUsers] = useState([]);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    fetch("http://localhost:8000/users")
+      .then((response) => response.json())
+      .then((data) => setUsers(data))
+      .catch((error) => console.error(error));
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const isValid = validateForm();
     if (isValid) {
-      console.log("Sign in successful");
-      navigate("/");
+      const user = users.find(
+        (user) => user.email === email && user.password === password
+      );
+      if (user) {
+        console.log("Sign in successful");
+        navigate("/");
+      } else {
+        setLoginError("Invalid email or password");
+      }
     }
   };
 
@@ -68,6 +84,7 @@ const SignIn = () => {
               />
               {passwordError && <p className="error">{passwordError}</p>}
             </div>
+            {loginError && <p className="error">{loginError}</p>} 
             <p>
               By signing up, you confirm that you've read and accepted our User
               Notice and Privacy Policy.
