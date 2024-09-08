be07a2a1cf4b16f97358c262a1ed26998c9455d7
KiwiScreams <screamskiwi3@gmail.com>
2024-09-08 16:20:46 +0400

    logout

--- src/components/shared/header/Header.jsx
+++ src/components/shared/header/Header.jsx
@@ -1,5 +1,6 @@
 import "./Header.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 import logo from "../../../../public/logo.svg.svg";
 import Search from "./search/Search";
 import iconCart from "../../../assets/icons/header icons/icon-cart.svg.svg";
@@ -7,32 +8,43 @@ import iconWishlist from "../../../assets/icons/header icons/icon-heart.svg.svg"
 import iconAccount from "../../../assets/icons/header icons/icon-user.svg.svg";
 import iconRe from "../../../assets/icons/header icons/icon-re.svg.svg";
 import Nav from "./nav/Nav";
-const headerLinks = [
-  {
-    path: "/compare",
-    name: "Compare",
-    image: iconRe,
-    quantity: 0,
-  },
-  {
-    path: "/wishlist",
-    name: "Wishlist",
-    image: iconWishlist,
-    quantity: 0,
-  },
-  {
-    path: "/cart",
-    name: "Cart",
-    image: iconCart,
-    quantity: 0,
-  },
-  {
-    path: "/signup",
-    name: "Account",
-    image: iconAccount,
-  },
-];
+
 const Header = () => {
+  const navigate = useNavigate();
+  const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
+
+  const handleLogout = () => {
+    localStorage.removeItem("isLoggedIn");
+    localStorage.removeItem("userEmail");
+    navigate("/signin");
+  };
+
+  const headerLinks = [
+    {
+      path: "/compare",
+      name: "Compare",
+      image: iconRe,
+      quantity: 0,
+    },
+    {
+      path: "/wishlist",
+      name: "Wishlist",
+      image: iconWishlist,
+      quantity: 0,
+    },
+    {
+      path: "/cart",
+      name: "Cart",
+      image: iconCart,
+      quantity: 0,
+    },
+    {
+      path: isLoggedIn ? "/" : "/signup",
+      name: isLoggedIn ? "Logout" : "Account",
+      image: iconAccount,
+    },
+  ];
+
   return (
     <>
       <header>
@@ -43,7 +55,11 @@ const Header = () => {
                 <Link to="/about">About Us</Link>
               </li>
               <li>
-                <Link to="/signup">My Account</Link>
+                {isLoggedIn ? (
+                  <a onClick={handleLogout}>Logout</a>
+                ) : (
+                  <Link to="/signup">My Account</Link>
+                )}
               </li>
               <li>
                 <Link to="/wishlist">Wishlist</Link>
@@ -64,7 +80,7 @@ const Header = () => {
         </div>
         <div className="header-middle">
           <div className="header-middle-container">
-            <img src={logo} alt="Nest" className="logo"/>
+            <img src={logo} alt="Nest" className="logo" />
             <Search />
             <ul>
               {headerLinks.map((link, index) => (
