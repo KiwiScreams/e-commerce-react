4aba71c2f4ed731c8d6fdc553dde4633905a3e90
KiwiScreams <screamskiwi3@gmail.com>
2024-09-08 19:44:32 +0400

    navigation styles

--- src/components/admin components/navigation/Navigation.jsx
+++ src/components/admin components/navigation/Navigation.jsx
@@ -1,16 +1,28 @@
 import "./Navigation.css";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 const Navigation = () => {
-    return ( 
-        <>
-        <section className="admin-panel-navigation">
-            <ul>
-                <li><Link to="/admin/products">products</Link></li>
-                <li><Link to="/admin/add">add</Link></li>
-            </ul>
-        </section>
-        </>
-     );
-}
- 
+  return (
+    <>
+      <section className="admin-panel-navigation">
+        <ul>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/admin/products">Products</NavLink>
+          </li>
+          <li>
+            <NavLink to="/admin/add">Add new product</NavLink>
+          </li>
+        </ul>
+      </section>
+    </>
+  );
+};
+
 export default Navigation;
