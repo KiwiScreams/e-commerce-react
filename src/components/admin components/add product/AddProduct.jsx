58b83b1abaf0dc50aa080d136fe24ddc57e11187
KiwiScreams <screamskiwi3@gmail.com>
2024-09-08 14:02:39 +0400

    create product

--- src/components/admin components/add product/AddProduct.jsx
+++ src/components/admin components/add product/AddProduct.jsx
@@ -123,7 +123,213 @@ const AddProduct = () => {
   };
 
   return (
-  
+    <section className="add-product-section">
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+          <label htmlFor="id">ID:</label>
+          <input
+            type="text"
+            name="id"
+            placeholder="Enter product ID"
+            value={inputData.id}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="input-container">
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter product name"
+            value={inputData.name}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="input-container">
+          <label htmlFor="description">Description:</label>
+          <textarea
+            name="description"
+            placeholder="Enter product description"
+            value={inputData.description}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="input-container">
+          <label htmlFor="discount">Discount:</label>
+          <input
+            type="number"
+            name="discount"
+            step="0.01"
+            placeholder="Enter product discount"
+            value={inputData.discount}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="input-container">
+          <label htmlFor="price">Price:</label>
+          <input
+            type="number"
+            step={0.01}
+            name="price"
+            placeholder="Enter product price"
+            value={inputData.price}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="input-container">
+          <label htmlFor="image">Image:</label>
+          <input
+            type="text"
+            name="image"
+            placeholder="Enter product image URL"
+            value={inputData.image}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="input-container">
+          <label htmlFor="catagory">Category:</label>
+          <input
+            type="text"
+            name="catagory"
+            placeholder="Enter product category"
+            value={inputData.catagory}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="input-container">
+          <label htmlFor="color">Color</label>
+          <input
+            type="text"
+            name="color"
+            placeholder="Enter product color"
+            value={inputData.color}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="input-container">
+          <label htmlFor="more">More:</label>
+          <input
+            type="text"
+            name="more"
+            placeholder="Enter more information"
+            value={inputData.more}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="input-container">
+          <label htmlFor="rating">Rating:</label>
+          <input
+            type="number"
+            name="rating"
+            placeholder="Enter product rating"
+            value={inputData.rating}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="input-container">
+          <label htmlFor="company">Company:</label>
+          <input
+            type="text"
+            name="company"
+            placeholder="Enter product company"
+            value={inputData.company}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="input-container">
+          <label>Sizes:</label>
+          {inputData.sizes.map((size, index) => (
+            <input
+              key={index}
+              type="text"
+              value={size}
+              onChange={(event) => handleSizeChange(event, index)}
+            />
+          ))}
+          <button type="button" onClick={addSize}>
+            Add Size
+          </button>
+        </div>
+        <div className="input-container">
+          <label>Additional Info:</label>
+          {inputData.additionalInfo.map((info, index) => (
+            <div key={index}>
+              <label>Description:</label>
+              <input
+                type="text"
+                value={info.description}
+                onChange={(event) => handleAdditionalInfoChange(event, index)}
+                name="description"
+              />
+              <label>Details:</label>
+              {info.details.map((detail, subIndex) => (
+                <div key={subIndex}>
+                  <label>Type of Packing:</label>
+                  <input
+                    type="text"
+                    value={detail.typeOfPacking}
+                    onChange={(event) =>
+                      handleAdditionalInfoChange(event, index, subIndex)
+                    }
+                    name="typeOfPacking"
+                  />
+                  <label>Color:</label>
+                  <input
+                    type="text"
+                    value={detail.color}
+                    onChange={(event) =>
+                      handleAdditionalInfoChange(event, index, subIndex)
+                    }
+                    name="color"
+                  />
+                  <label>Quantity Per Case:</label>
+                  <input
+                    type="text"
+                    value={detail.quantityPerCase}
+                    onChange={(event) =>
+                      handleAdditionalInfoChange(event, index, subIndex)
+                    }
+                    name="quantityPerCase"
+                  />
+                </div>
+              ))}
+              <label>Packaging Delivery:</label>
+              <input
+                type="text"
+                value={info.PackagingDelivery}
+                onChange={(event) => handleAdditionalInfoChange(event, index)}
+                name="PackagingDelivery"
+              />
+              <label>Suggested Use:</label>
+              <input
+                type="text"
+                value={info.SuggestedUse}
+                onChange={(event) => handleAdditionalInfoChange(event, index)}
+                name="SuggestedUse"
+              />
+              <label>Ingredients:</label>
+              <input
+                type="text"
+                value={info.Ingredients}
+                onChange={(event) => handleAdditionalInfoChange(event, index)}
+                name="Ingredients"
+              />
+              <label>Warnings:</label>
+              <input
+                type="text"
+                value={info.warnings}
+                onChange={(event) => handleAdditionalInfoChange(event, index)}
+                name="warnings"
+              />
+            </div>
+          ))}
+          <button type="button" onClick={addAdditionalInfo}>
+            Add Additional Info
+          </button>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </section>
   );
 };
 
