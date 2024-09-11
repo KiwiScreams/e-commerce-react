3079c8645665986307d2d93440230a0a79e94cf6
KiwiScreams <screamskiwi3@gmail.com>
2024-09-11 19:51:01 +0400

    success panel

--- src/components/success panel/SuccessPanel.jsx
+++ src/components/success panel/SuccessPanel.jsx
@@ -1,10 +1,13 @@
 import "./SuccessPanel.css";
-const SuccessPanel = (props) => {
-    return ( 
-        <>
-        {props}
-        </>
-     );
-}
- 
+const SuccessPanel = ({ email }) => {
+  return (
+    <>
+      <div className="success-panel">
+        <h2>Success!</h2>
+        <p>Your email is: {email}</p>
+      </div>
+    </>
+  );
+};
+
 export default SuccessPanel;
